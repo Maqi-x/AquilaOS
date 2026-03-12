16cc1f957b6e49cad92592189ba935c8d2e37272
Gorciu <122084593+gorciu-official@users.noreply.github.com>
2026-03-12 15:53:21 +0100

    fix building

--- src/drivers/misc/memory.cpp
+++ src/drivers/misc/memory.cpp
@@ -103,4 +103,6 @@ void* operator new[](size_t size) { return malloc(size); }
 
 void operator delete(void* ptr) noexcept { free(ptr); }
 
+void operator delete(void* ptr, unsigned int) noexcept { free(ptr); }
+
 void operator delete[](void* ptr) noexcept { free(ptr); }
